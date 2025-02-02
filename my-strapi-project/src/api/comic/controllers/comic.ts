08e9dc4d89f0f561a6bd2498e9a8fff86a36ab9c
M-Bim <137218901+M-Bim@users.noreply.github.com>
2025-02-02 15:05:45 -0500

    removing strapi and choosing to use directus for cms

--- my-strapi-project/src/api/comic/controllers/comic.ts
+++
@@ -1,7 +0,0 @@
-/**
- * comic controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::comic.comic');
--- my-strapi-project/src/api/comic/routes/comic.ts
+++
@@ -1,7 +0,0 @@
-/**
- * comic router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::comic.comic');
